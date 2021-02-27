83a60debac5dd05a8421bd6148baaf98e0931a69
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-02-27 23:00:04 +0300

    all

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,11 @@
 from flask import render_template, request, redirect, url_for, abort
 from . import main
+from .. import db,photos
 from .forms import PitchForm, CommentForm, BioForm
 from flask_login import login_required,current_user
+from ..models import User, Pitch, Comment
+from flask import jsonify
+from multiprocessing import Value
 
 @main.route('/')
 def index():
@@ -20,7 +24,7 @@ def new_pitch():
             return redirect(url_for('main.product_pitches'))
 
         elif category == 'Marketing':
-            return redirect(url_for('main.Marketing_pitches'))
+            return redirect(url_for('main.marketing_pitches'))
         
         elif category == 'Punch Lines':
             return redirect(url_for('main.punch_lines'))
@@ -33,3 +37,26 @@ def new_pitch():
 
     return render_template('new_pitch.html', review_form=form)
 
+@main.route('/pitches/product_pitches')
+def product_pitches():
+    pitch = Pitch.query.all()
+    product = Pitch.query.filter_by(category='Product').all()
+    return render_template('product.html',product=product)
+
+@main.route('/pitches/marketing_pitches')
+def marketing_pitches():
+    pitch = Pitch.query.all()
+    marketing = Pitch.query.filter_by(category='Marketing').all()
+    return render_template('marketing.html',marketing=marketing)
+
+@main.route('/pitches/punch_lines')
+def punch_lines():
+    pitch = Pitch.query.all()
+    punchlines = Pitch.query.filter_by(category='Punchlines').all()
+    return render_template('punch_lines.html',punchlines=punchlines)
+
+@main.route('/pitches/pickup_lines')
+def pickup_lines():
+    pitch = Pitch.query.all()
+    pickuplines = Pitch.query.filter_by(category='Pickup Lines').all()
+    return render_template('pickup_lines.html',pickuplines=pickuplines)
--- manage.py
+++ manage.py
@@ -1,8 +1,19 @@
+from app import create_app,db
+from app.models import User, Pitch, Comment
 from app import app
 from flask_script import Manager,Server
+from flask_migrate import Migrate, MigrateCommand
 
+app = create_app('production')
 manager = Manager(app)
 manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app,db=db,user=User,Pitch=Pitch,Comment=Comment)
 
 manager.add_command('server', Server)
 @manager.command
