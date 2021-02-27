df9f6d53c95d9c5b7116d4dbddd014d49557baf9
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-02-27 23:05:04 +0300

    add routes

--- app/main/views.py
+++ app/main/views.py
@@ -60,3 +60,101 @@ def pickup_lines():
     pitch = Pitch.query.all()
     pickuplines = Pitch.query.filter_by(category='Pickup Lines').all()
     return render_template('pickup_lines.html',pickuplines=pickuplines)
+
+
+@main.route('/pitches/comments/<int:pitch_id>', methods=['GET','POST'])
+@login_required
+def leave_comment(pitch_id):
+    comment_form = CommentForm()
+    pitches = Pitch.query.get(pitch_id)
+    comment = Comment.query.filter_by(pitch_id=pitch_id).all()
+    if comment_form.validate_on_submit():
+        comments = comment_form.comment.data
+
+        pitch_id= pitch_id
+        user_id = current_user._get_current_object().id
+        new_comment= Comment(comments=comments,pitch_id=pitch_id, user_id=user_id)
+        new_comment.save_comment() 
+
+        return redirect(url_for('main.pitch_page',comment_form=comment_form,pitch_id=pitch_id))
+        
+    return render_template('new_comment.html',comment_form=comment_form, comment=comment,pitch_id=pitch_id) 
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    user_id = current_user.id
+    pitch = Pitch.query.filter_by(user_id=user_id).all()
+
+    if user is None:
+        abort(404)
+        
+    return render_template("profile/profile.html", user = user, pitch=pitch)
+
+@main.route('/user/<uname>/pitches',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    
+    if user is None:
+        abort(404)
+
+    form = PitchForm()
+
+    if form.validate_on_submit():
+        pitch = form.my_pitches.data
+        category = form.my_category.data
+        
+        new_pitch=Pitch(pitch=pitch,category=category,user_id=current_user.id)
+        
+        new_pitch.save_pitch()
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/bio',methods = ['GET','POST'])
+@login_required
+def update_bio(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    bioform = BioForm()
+
+    if bioform.validate_on_submit():
+        user.bio = bioform.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/bio.html',bioform=bioform)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
+@main.route('/pitches')
+def pitch_page():    
+    user = User.query.all()
+    pitches = Pitch.query.all()
+    user=current_user
+    return render_template('pitches.html',pitches=pitches,user=user)
+
+@main.route('/upvote/<int:like>/<int:pitch_id>')
+def increment(like,pitch_id):
+    all_likes = Pitch.query.filter_by(like,pitch_id).all()
+    likes = int(all_likes) + 1
+    user_id = user_id
+    new_like = Pitch(user_id=user_id,pitch_id=pitch_id,likes=like)
+    new_like.save_pitch()
+    
+    return render_template('pitches.html',likes=likes)             
