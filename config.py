fd78ee9163c9960eadd2fb93d2b467c13f756ad1
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-02-28 13:42:02 +0300

    all

--- config.py
+++ config.py
@@ -1,11 +1,33 @@
 import os
 
 class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Moringa@localhost/pitches'
+    SECRET_KEY = 'eVwLKGWwa3Io7sT4YQB41A'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    pass
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Moringa@localhost/pitches_test'
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Moringa@localhost/pitches'
+    DEBUG = True
     
-
-
 config_options = {
     'development':DevConfig,
     'production':ProdConfig,
     'test':TestConfig
 }
+    
+    
+
+
+ 
